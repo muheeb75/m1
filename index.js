d2aa50991b62c516a91afb87db226039bac4a083
Mohammed Uzair <muheeb.pasha@aqxolt.com>
2021-10-26 19:10:04 +0530

    Add cool face API15

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ const SellingPartnerAPI = require('amazon-sp-api');
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM test_table');
     const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
+    //res.render('pages/db', results );
     client.release();
   } catch (err) {
     console.error(err);
