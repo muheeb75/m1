d9c5c2fb8e8921856ccd257aa08cf47086e467d4
Mohammed Uzair <muheeb.pasha@axolt.com>
2021-11-08 16:55:50 +0530

    Add cool face API10

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ const e = require('express');
         var productList = [];
         var mainProductList = [];
         productList.push(result.rows);
-        res.send(JSON.stringify(OrderItemsList[0].OrderItemId));
+        res.send(JSON.stringify(OrderItemsList[0].OrderItemId + productList[0].ERP7__OrderItemId__c));
         //res.send(`<h1>${JSON.stringify(productList.rows)}<h1>`);
        /* if(productList != [] && OrderItemsList != []){
           for(let i in productList){
